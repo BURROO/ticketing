4fc9d8dac03ffe351b10e946b4ddb8ad048a9f3d
Philipp Koller <philipp.kooller@googlemail.com>
2021-05-09 12:58:35 +0200

    update start message

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -9,7 +9,7 @@ import { PaymentCreatedListener } from './events/listener/payment-created-listen
 
 const start = async () => {
 
-  console.log('Starting ...')
+  console.log('Starting ....')
 
   if(!process.env.JWT_KEY){
     throw new Error('JWT_KEY must be defined')
